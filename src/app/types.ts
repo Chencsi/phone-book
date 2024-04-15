3418a945fbaaced80be9e7bc95b4d23c9ba360d6
Chen Kevin <diak_344.chen.kevin@12.c.2023.com>
2024-04-15 13:55:19 +0200

    Contact interface added

---
+++ src/app/types.ts
@@ -0,0 +1,11 @@
+export interface Contact {
+    id: number
+    name: string
+    phone: string
+    email: string
+    birth: string
+    company: string
+    role: string
+    address: string
+    notes: string
+}
