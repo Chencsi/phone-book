4f00a1ffe79ab9a756b86446ceda5d017224665e
Chen Kevin <kevinkeeeh@gmail.com>
2024-04-16 21:10:19 +0200

    Navbar added, but "add new" not works yet

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  public currentRoute: any;
+  constructor(private router: Router) {
+    this.currentRoute = this.router.url;
+  }
 }
