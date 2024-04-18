111366e58081c200e7abb1b16a8386c0e4e49f11
Chen Kevin <kevinkeeeh@gmail.com>
2024-04-18 09:51:40 +0200

    Form control on new page

--- src/app/components/add-new/add-new.component.ts
+++ src/app/components/add-new/add-new.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DbService } from '../../db.service';
+import { Contact } from '../../types';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-new',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-new.component.html',
   styleUrl: './add-new.component.scss'
 })
 export class AddNewComponent {
+  public db: DbService;
+  public router: Router;
+  contactForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ])
+  });
+  constructor() {
+    this.db = inject(DbService);
+    this.router = inject(Router);
+    this.db.onNewContactAdded.subscribe(() => {
+      this.router.navigate(['home'])
+    })
+  }
 
+  onSubmit(): void {
+    const value = this.contactForm.value;
+    const contact = {
+      name: value.name as string,
+      phone: value.phone as string,
+      email: value.email as string
+    }
+    this.db.addContact(contact)
+  }
 }
